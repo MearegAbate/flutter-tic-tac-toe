75a501177ad2d44f5f7a83317d0d088d54dc835c
unknown <mearegabate@gmail.com>
2019-05-30 05:21:27 -0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'pages/app.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Game',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyApp(),
+    ));
